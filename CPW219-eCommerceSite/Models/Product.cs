385959742e4b60cd8f7a8415b2457ea0a44e7cf4
JoeDLehmanJr <lehman5247@students.cptc.edu>
2022-07-31 15:05:28 -0700

    Added Product class.

---
+++ CPW219-eCommerceSite/Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219_eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents an individual software available for purchase
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// The unique identifier for each software product
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The title of the software program
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The sale price of the software program
+        /// </summary>
+        [Range(0, 9999)]
+        public decimal Price { get; set; }
+
+
+        /// <summary>
+        /// The category of the software eg. Operating System, Office, and security, to name a few.
+        /// </summary>
+        [Required]
+        public string Category { get; set; }
+
+        /// <summary>
+        /// The product description
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+
+    }
+}
