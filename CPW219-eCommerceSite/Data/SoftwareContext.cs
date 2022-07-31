ca1d5dd5f68013e5741b729202ea0a0a8aa2dce5
JoeDLehmanJr <lehman5247@students.cptc.edu>
2022-07-31 16:11:31 -0700

    Added Db Context class.

---
+++ CPW219-eCommerceSite/Data/SoftwareContext.cs
@@ -0,0 +1,14 @@
+﻿using CPW219_eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW219_eCommerceSite.Data
+{
+    public class SoftwareContext : DbContext    
+    {
+        public SoftwareContext(DbContextOptions<SoftwareContext> options ) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
