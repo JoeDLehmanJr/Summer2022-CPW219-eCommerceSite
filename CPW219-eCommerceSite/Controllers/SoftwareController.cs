86f5b11c255b1d52350aa8e6d4b0a8a55927d334
JoeDLehmanJr <lehman5247@students.cptc.edu>
2022-08-07 16:09:27 -0700

    Create software fully functional, added some styling.

--- CPW219-eCommerceSite/Controllers/SoftwareController.cs
+++ CPW219-eCommerceSite/Controllers/SoftwareController.cs
@@ -1,13 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class SoftwareController : Controller
     {
+        private readonly SoftwareContext _context;
+
+        public SoftwareController(SoftwareContext context)
+        {
+            _context = context;
+        }
         [HttpGet]
         public IActionResult Create() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                // Prepares Insert
+                _context.Products.Add(product);
+
+                // add to DB
+                _context.SaveChanges();
+                ViewData["Message"] = $"{product.Title} has been added to the database";
+
+                // Show success message on page
+                // return RedirectToAction("Index", "Home");
+
+                return View();
+            }
+            return View(product);
         }
     }
 }
