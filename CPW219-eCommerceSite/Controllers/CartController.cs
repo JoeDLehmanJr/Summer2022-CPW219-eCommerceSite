469a529897cb4fc9906548f6e4e9b3af63da554f
JoeDLehmanJr <lehman5247@students.cptc.edu>
2022-08-31 11:47:32 -0700

    Add shopping cart button to th software catalog page.

---
+++ CPW219-eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly SoftwareContext _context;
+
+        public CartController(SoftwareContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Product? productToAdd = _context.Product.Where( s => s.ProductId == id ).SingleOrDefault();
+
+            if(productToAdd == null)
+            {
+                // Software with specified id doesn't exist
+                TempData["Message"] = "Sorry that software doesn't exists";
+                return RedirectToAction("Index", "Software");
+            }
+
+            // ToDo: Add item to a shopping cart cookie
+            TempData["Message"] = "Item added to the cart.";
+            return RedirectToAction("Index", "Software");
+        }
+    }
+}
